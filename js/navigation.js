f48c1380bfeb51189731f62fdb65d9698ea30593
Félix Bouveret <bouveret.felix@gmail.com>
2020-01-31 22:16:35 +0100

    Add auto navigation generation

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,31 @@
 
-const body = document.querySelector('body');
-let head
-const seanceNbr = 4;
+const seanceNbr = 6;
+const body = document.querySelector('.canvasContainer');
+let header
+let list
+let nav
+let listItem
+let link
+let linkText 
 
 head = document.createElement('p');
+header = document.createElement("header")
+nav = document.createElement("nav")
+list = document.createElement("ul")
+nav.appendChild(list)
+header.appendChild(nav)
 
-for(let i = 1; i < 4; i++) {
-  body.insertAdjacentHTML("afterbegin", '<li><a href="/seance' + i + '">Séance' + i + '</a></li>');
+for(let i = 1; i <= seanceNbr; i++) {
+  listItem = document.createElement("li")
+
+  link = document.createElement("a")
+  link.setAttribute("href", '/seance' + i )
+
+  linkText = document.createTextNode('Séance - ' + i ); 
+
+
+  link.appendChild(linkText)
+  listItem.appendChild(link)
+  list.appendChild(listItem)
 }
+document.querySelector('.wrapper').insertBefore(header, body)
